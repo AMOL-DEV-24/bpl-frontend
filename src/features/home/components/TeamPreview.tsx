dd20dfb49b43f9c44562ff14aab3ea700c24e343
Amol Pawar <amolpawar.dev@gmail.com>
2026-06-12 11:04:04 +0530

    Add team images and update TeamsPreview component for enhanced display; improve styles for better responsiveness and organization

--- src/features/home/components/TeamPreview.tsx
+++ src/features/home/components/TeamPreview.tsx
@@ -1,8 +1,41 @@
+import Image from "next/image";
+
 const teams = [
-  "Warriors",
-  "Kings",
-  "Titans",
-  "Strikers",
+  {
+    id: 1,
+    name: "Linge Super Kings",
+    image: "/assets/images/teams/Linge Super Kings 1.png",
+  },
+  {
+    id: 2,
+    name: "Dayavan 11",
+    image: "/assets/images/teams/Dayavan 11 - 1.png",
+  },
+  {
+    id: 3,
+    name: "Gajanan Riders",
+    image: "/assets/images/teams/Gajanan Riders 2.png",
+  },
+  {
+    id: 4,
+    name: "Irfan Fighters",
+    image: "/assets/images/teams/Irfan Fighters 2.png",
+  },
+  {
+    id: 5,
+    name: "Sadguru Lions",
+    image: "/assets/images/teams/Sadguru Lions.png",
+  },
+  {
+    id: 6,
+    name: "Samrat Yodha",
+    image: "/assets/images/teams/Samrat Yodha 1.png",
+  },
+  {
+    id: 7,
+    name: "Mahalaxmi Warriors",
+    image: "/assets/images/teams/Mahalaxmi Warriors.png"
+  }
 ];
 
 export default function TeamsPreview() {
@@ -12,21 +45,33 @@ export default function TeamsPreview() {
 
         <div className="section-heading">
           <h2>Participating Teams</h2>
+
+          <p>
+            Official teams competing in the
+            Bhalawani Premier League.
+          </p>
         </div>
 
         <div className="teams-grid">
 
           {teams.map((team) => (
-            <div
-              key={team}
+            <article
+              key={team.id}
               className="team-card"
             >
-              <div className="team-logo">
-                🏏
+              <div className="team-image">
+                <Image
+                  src={team.image}
+                  alt={team.name}
+                  fill
+                  className="team-img"
+                />
               </div>
 
-              <h3>{team}</h3>
-            </div>
+              <div className="team-content">
+                <h3>{team.name}</h3>
+              </div>
+            </article>
           ))}
 
         </div>
